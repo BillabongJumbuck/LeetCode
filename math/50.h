0bc8e62560c1ac0a92d74e5521ac84072797b681
piglet <2792904471@qq.com>
2024-12-31 00:03:36 +0800

    241231

--- math/50.h
+++ math/50.h
@@ -4,16 +4,35 @@
 
 #ifndef LEETCODE_50_H
 #define LEETCODE_50_H
+
+#include <climits>
+
 class Solution {
 private:
     double pow_iter(double x, int n, double res){
-        return n == 0 ? res :
-                n%2 ? pow_iter(x, n-1, res + x)
-                 : pow_iter(x * x, n/2, res);
+        if(n == 0){
+            return res;
+        }
+        if(n%2){
+            return pow_iter(x, n-1, res * x);
+        }else{
+            return pow_iter(x * x, n/2, res);
+        }
     }
 public:
     double myPow(double x, int n) {
-        return pow_iter(x, n, 0);
+        if(n == 0){
+            return 1;
+        }else if(x == 1){
+            return x;
+        }else if(n > 0){
+            return pow_iter(x, n, 1);
+        }else if(n == INT_MIN){
+            return 1/(pow_iter(x, -(n+1), 1) * x);
+        }
+        else{
+            return 1/pow_iter(x, -n, 1);
+        }
     }
 };
 #endif //LEETCODE_50_H
